8b1da6a64563b618a14e64577d2e184185773bdc
Mariusz Zakrzewski <mzakrze1@gmail.com>
2021-08-26 20:28:22 +0200

    Plot diameter

--- image-recognition.py
+++ image-recognition.py
@@ -4,8 +4,13 @@ import os
 import matplotlib.pyplot as plt
 
 def get_list_all_images():
-    return sorted(os.listdir('images'), key=lambda x: int(x[:-len('.PNG')]))
+    return sorted(os.listdir('images'), key=image_name_to_number)
 
+def image_name_to_number(image_name):
+    return int(image_name[:-len('.PNG')])
+
+image_no = []
+image_diameter = []
 
 for x in get_list_all_images():
     img = cv2.imread('images/' + str(x))
@@ -20,6 +25,12 @@ for x in get_list_all_images():
     diameter = 2 * radius
     print('Diameter {}: {}'.format(x, diameter))
 
+    image_no.append(image_name_to_number(x))
+    image_diameter.append(diameter)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+
+plt.plot(image_no, image_diameter)
+plt.show()
